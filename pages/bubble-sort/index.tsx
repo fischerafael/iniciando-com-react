643e5f3315efa15d0a333ecafa5351ed97daaaa7
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-12-07 21:46:27 -0300

    feat: buble sort working

--- pages/bubble-sort/index.tsx
+++ pages/bubble-sort/index.tsx
@@ -5,17 +5,30 @@ const index = () => {
     8, 5, 2, 0, 3, 1, 12, 11, 52, 33, 123123, 23,
   ]);
 
+  const [value, setValue] = useState(0);
+
   const orderedNumbers = bubbleSort(numbers);
+
+  const handleAdd = () => {
+    if (value === 0) return;
+    if (numbers.includes(value)) return;
+    setNumbers((state) => [...state, value]);
+    setValue(0);
+  };
 
   return (
     <div>
       <div>
-        <input type="number" />
-        <button>Add</button>
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => setValue(() => +e.target.value)}
+        />
+        <button onClick={handleAdd}>Add</button>
       </div>
       <div>
-        {orderedNumbers.map((num) => (
-          <li key={num}>{num}</li>
+        {orderedNumbers.map((num, index) => (
+          <li key={index}>{num}</li>
         ))}
       </div>
     </div>
@@ -25,21 +38,14 @@ const index = () => {
 export default index;
 
 const bubbleSort = (numbers: number[]): number[] => {
-  let lastItemsAlreadyOrdered = 0;
-
   const orderedNumbers = [...numbers];
-  for (
-    let j = 0;
-    j < orderedNumbers.length - 1 - lastItemsAlreadyOrdered;
-    j++
-  ) {
+  for (let j = 0; j < orderedNumbers.length - 1; j++) {
     for (let i = 0; i < orderedNumbers.length - 1; i++) {
       const current = orderedNumbers[i];
       const next = orderedNumbers[i + 1];
       if (current > next) {
         orderedNumbers[i] = next;
         orderedNumbers[i + 1] = current;
-        lastItemsAlreadyOrdered++;
       }
     }
   }
